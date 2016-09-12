62980ba4555930fa27fce0124a48a88f3f4aca90
Rey Garcia <reynaldogarcia@Reynaldos-iMac.local>
2016-09-12 09:06:23 -0600

    auth

--- index.js
+++ index.js
@@ -7,73 +7,143 @@ var app = express();
 
 var jsonParser = bodyParser.json();
 
-app.post('/users', jsonParser, function(req, res) {
-    if (!req.body) {
-        return res.status(400).json({
-            message: "No request body"
-        });
-    }
-
-    if (!('username' in req.body)) {
-        return res.status(422).json({
-            message: 'Missing field: username'
-        });
-    }
-
-    var username = req.body.username;
-
-    if (typeof username !== 'string') {
-        return res.status(422).json({
-            message: 'Incorrect field type: username'
-        });
-    }
-
-    username = username.trim();
-
-    if (username === '') {
-        return res.status(422).json({
-            message: 'Incorrect field length: username'
-        });
-    }
-
-    if (!('password' in req.body)) {
-        return res.status(422).json({
-            message: 'Missing field: password'
-        });
-    }
-
-    var password = req.body.password;
-
-    if (typeof password !== 'string') {
-        return res.status(422).json({
-            message: 'Incorrect field type: password'
-        });
-    }
-
-    password = password.trim();
-
-    if (password === '') {
-        return res.status(422).json({
-            message: 'Incorrect field length: password'
-        });
-    }
-
-    var user = new User({
-        username: username,
-        password: password
+app.post('/users', jsonParser, function (req, res) {
+  if (!req.body) {
+    return res.status(400).json({
+      message: 'No request body'
     });
+  }
 
-    user.save(function(err) {
+  if (!('username' in req.body)) {
+    return res.status(422).json({
+      message: 'Missing field: username'
+    });
+  }
+
+  var username = req.body.username;
+
+  if (typeof username !== 'string') {
+    return res.status(422).json({
+      message: 'Incorrect field type: username'
+    });
+  }
+
+  username = username.trim();
+
+  if (username === '') {
+    return res.status(422).json({
+      message: 'Incorrect field length: username'
+    });
+  }
+
+  if (!('password' in req.body)) {
+    return res.status(422).json({
+      message: 'Missing field: password'
+    });
+  }
+
+  var password = req.body.password;
+
+  if (typeof password !== 'string') {
+    return res.status(422).json({
+      message: 'Incorrect field type: password'
+    });
+  }
+
+  password = password.trim();
+
+  if (password === '') {
+    return res.status(422).json({
+      message: 'Incorrect field length: password'
+    });
+  }
+
+  var user = new User({
+    username: username,
+    password: password
+  });
+
+  user.save(function (err) {
+    if (err) {
+      return res.status(500).json({
+        message: 'Internal server error'
+      });
+    }
+
+    return res.status(201).json({});
+  });
+});
+
+var bcrypt = require('bcryptjs');
+
+    bcrypt.genSalt(10, function(err, salt) {
         if (err) {
             return res.status(500).json({
                 message: 'Internal server error'
             });
         }
 
-        return res.status(201).json({});
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal server error'
+                });
+            }
+
+            var user = new User({
+                username: username,
+                password: hash
+            });
+
+            user.save(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Internal server error'
+                    });
+                }
+
+                return res.status(201).json({});
+            });
+        });
+    });
+
+mongoose.connect('mongodb://localhost/auth').then(function () {
+  app.listen(8080);
+});
+
+
+
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+
+var strategy = new BasicStrategy(function(username, password, callback) {
+    User.findOne({
+        username: username
+    }, function (err, user) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (!user) {
+            return callback(null, false, {
+                message: 'Incorrect username.'
+            });
+        }
+
+        user.validatePassword(password, function(err, isValid) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (!isValid) {
+                return callback(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return callback(null, user);
+        });
     });
 });
 
-mongoose.connect('mongodb://localhost/auth').then(function() {
-    app.listen(8080);
-});
+passport.use(strategy);
